8111aa7da52a49f51d0714175633893b20368ab3
dianashyian88 <dvshyian@gmail.com>
2023-11-12 19:46:49 +0300

    Initial commit

---
+++ distribution/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from distribution.apps import DistributionConfig
+from django.views.decorators.cache import cache_page
+
+app_name = DistributionConfig.name
+
+urlpatterns = [
+    #path('', HomeListView.as_view(), name='home'),
+    #path('contacts/', contacts, name='contacts'),
+    #path('categories/', categories, name='categories'),
+
+
+]
